237d4bc14203a26a52ec05aef91ac7e0362859a4
PhHeinzenFurb <phheinzen@furb.br>
2025-06-10 13:00:26 -0300

    Criados mapa-mundi.R e mapa-europa.R
    Códigos com a criação dos mapas da europa e do mundo

---
+++ mapa-europa.R
@@ -0,0 +1,50 @@
+library(wbstats)
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(gganimate)
+library(gifski)
+
+# códigos a serem utilizados
+# Exportações e Importações de Bens e Serviços (% do PIB) 
+# (NE.EXP.GNFS.ZS,NE.IMP.GNFS.ZS)
+
+my_ids <- c("Exportacao (%PIB)" = "NE.EXP.GNFS.ZS",
+            "Importacao (%PIB)" = "NE.IMP.GNFS.ZS")
+
+# extraindo dados do banco mundial
+exp_imp_df <- wb_data(
+  indicator = my_ids,
+  country = "countries_only",
+  start_date = 1980, end_date = 2023
+)
+
+# extraindo dados geoespaciais dos paises
+europe_map <- ne_countries(scale = "small", type = "countries",
+                          continent = "Europe") |>
+  select(iso_a2, name, subregion)
+
+# juntando exp_imp_df e world_map
+exp_imp_map_eu <- exp_imp_df |>
+  inner_join(world_map, by = c("iso2c" = "iso_a2")) |>
+  st_as_sf()
+
+# utilizando Lambert Conformal Conic para o mapa da europa
+exp_imp_map_lamb <- st_transform(exp_imp_map_eu, crs = 3035)
+
+# criando plot de mapa
+p <- exp_imp_map_lamb |>
+  filter(date >= 1985,
+         date <= 2000) |>
+  ggplot() +
+  geom_sf(aes(fill = `Importacao (%PIB)`)) +
+  labs(
+    title = "{current_frame}"
+  ) +
+  transition_manual(date)
+
+# ajustando o fps
+animate(p, fps = 10)
+
+# salavando o arquivo
+anim_save("europe.gif", a)
